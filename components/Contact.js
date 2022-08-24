69e4415eec4a2d33ecc24128182239915648052b
GMO <maksym.gordiyenko@hygge.software>
2022-08-24 21:44:09 +0300

    fix: emailJS public key

--- components/Contact.js
+++ components/Contact.js
@@ -7,7 +7,7 @@ const Contact = ({ social }) => {
   
   const sendEmails = (formData) => {
     emailjs
-      .send(process.env.EMAIL_JS_SERVICE_ID, process.env.EMAIL_JS_TEMPLATE, formData, process.env.EMAIL_JS_PUBLIC_KEY)
+      .send(process.env.EMAIL_JS_SERVICE_ID, process.env.EMAIL_JS_TEMPLATE, formData, 'hNf_fzRzIUoOqeR05')
       .then(result => console.log(result.text), error => console.log(error.text));
     reset();
   };
