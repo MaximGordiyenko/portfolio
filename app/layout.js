f367dde1e4ee59e24640e3f55db19333c167e87e
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2025-02-02 06:35:48 +0200

    feat: google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,35 @@
-import "../styles/globals.css";
 import { domAnimation, LazyMotion } from 'framer-motion';
+import "../styles/globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+                var f = d.getElementsByTagName(s)[0],
+                    j = d.createElement(s),
+                    dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+              })(window, document, 'script', 'dataLayer', 'GTM-MXQQ7C7K');
+            `
+        }}
+      />
+    </head>
     <LazyMotion features={domAnimation}>
-      <body>{children}</body>
+      <body>
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MXQQ7C7K"
+                height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+      </noscript>
+      {children}
+      </body>
     </LazyMotion>
     </html>
   );
